384ba693313941695f59e6c2c9d331ed11840fb0
Evergreen <evergreen.git@gmail.com>
2021-04-08 13:16:09 +0200

    Step 0: linenoise history support

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "linenoise.hpp"
+
 std::string READ(std::string input) { return input; }
 
 std::string EVAL(std::string input) { return input; }
@@ -8,22 +10,26 @@ std::string EVAL(std::string input) { return input; }
 std::string PRINT(std::string input) { return input; }
 
 std::string rep(std::string input) {
-    auto read = READ(input);
-    auto result = EVAL(read);
+    auto ast = READ(input);
+    auto result = EVAL(ast);
     auto output = PRINT(result);
     return output;
 }
 
 int main() {
 
+    const auto history_path = "history.txt";
+    linenoise::LoadHistory(history_path);
+    
+    std::string input;
     while (true) {
-
-        std::cout << "user> ";
-        std::string input;
-        if (!std::getline(std::cin, input))
+        auto quit = linenoise::Readline("hello> ", input);
+        if (quit)
             break;
         std::cout << rep(input) << std::endl;
+        linenoise::AddHistory(input.c_str());
     }
+    linenoise::SaveHistory(history_path);
 
     return 0;
 }
